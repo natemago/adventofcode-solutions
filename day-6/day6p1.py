fb72259be3ce819613045619e40ffde7d3b1e092
Pavle Jonoski <jonoski.pavle@gmail>
2016-09-20 15:37:17 +0200

    Solutions to days 6 and 7

---
+++ day-6/day6p1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import re
+
+
+class LightsGrid:
+  
+  def __init__(self, rows, cols):
+    self.grid = self._generate_grid(rows, cols)
+  
+  
+  def _generate_grid(self, rows, cols):
+    grid = []
+    for i in range(0, rows):
+      grid.append([0 for x in range(0, cols)])
+    return grid
+  
+  def turn_on(self, start, end):
+    for i in range(start[0], end[0]+1):
+      for j in range(start[1], end[1]+1):
+        self.grid[i][j] = 1
+  
+  def turn_off(self, start, end):
+    for i in range(start[0], end[0]+1):
+      for j in range(start[1], end[1]+1):
+        self.grid[i][j] = 0
+   
+  def toggle(self, start, end):
+    for i in range(start[0], end[0]+1):
+      for j in range(start[1], end[1]+1):
+        self.grid[i][j] = (self.grid[i][j]+1)%2
+    
+  def total_on(self):
+    t = [sum(row) for row in self.grid]
+    return sum(t)
+   
+
+grid = LightsGrid(1000, 1000)
+
+with open('input') as f:
+  line = f.readline()
+  while(line):
+    m = re.match('(?P<cmd>\w+\s*[^\d]*)\s(?P<start>\d+,\d+).*\s(?P<end>\d+,\d+)', line)
+    cmd = m.group('cmd').strip()
+    start = m.group('start')
+    end = m.group('end')
+    start = [int(s) for s in start.split(',')]
+    end = [int(s) for s in end.split(',')]
+    
+    if cmd == 'turn on':
+      grid.turn_on(start, end)
+    elif cmd == 'turn off':
+      grid.turn_off(start, end)
+    else:
+      grid.toggle(start, end)
+    
+    line = f.readline()
+
+  
+  
+print(grid.total_on())
+  
+  
+  
+  
---
+++ day-7/day7p2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+import re
+
+
+class Op:
+  def __init__(self, op, inputs, out):
+    self.inputs = inputs
+    self.out = out
+    self.op = op
+  
+  def resolve(self, wires):
+    print(' resolve %s with inputs %s' % (self.out, str(self.inputs)))
+    all_resolved = True
+    actual_inputs = []
+    for inp in self.inputs:
+      print ('   inp: %s' % str(inp))
+      if inp is None:
+        continue
+      if isinstance(inp, int):
+        actual_inputs.append(inp)
+        continue
+      
+      val = wires[inp]
+      if isinstance(val, Op):
+        val = val.resolve(wires)
+        if val is not None:
+          actual_inputs.append(val)
+        else:
+          all_resolved = False
+      else:
+        actual_inputs.append(val)
+    
+    if all_resolved:
+      try:
+        out = self._calc_out(actual_inputs)
+        wires[self.out] = out
+        print('%s = %d' % (self.out, out) )
+        return out
+      except:
+        print('Op -> %s [actual inputs: %s]' % (self, str(actual_inputs)))
+        raise
+    
+    return None
+  
+  def _calc_out(self, inputs):
+    if self.op is None:
+      return inputs[0]
+    if self.op == 'AND':
+      return inputs[0] & inputs[1]
+    if self.op == 'OR':
+      return inputs[0] | inputs[1]
+    if self.op == 'LSHIFT':
+      return inputs[0] << inputs[1]
+    if self.op == 'RSHIFT':
+      return inputs[0] >> inputs[1]
+    if self.op == 'NOT':
+      return ~inputs[0]
+  
+  def __str__(self):
+    return '%s %s' % (self.op, str(self.inputs))
+  
+  def __repr__(self):
+    return self.__str__()
+  
+wires = {}
+
+
+with open('input') as f:
+  line = f.readline()
+  while line:
+    print(line)
+    m = re.match('((?P<in1>\w+)\s+)?(?P<op>[A-Z]+)\s+(?P<in2>\w+)\s->\s(?P<target>\w+)', line)
+    if not m:
+      m = re.match('(?P<in1>\w+)\s+->\s+(?P<target>\w+)', line)
+      
+      
+    in1 = m.group('in1')
+    in2 = None
+    op = None
+    try:
+      in2 = m.group('in2')
+      op = m.group('op')
+    except:
+      pass
+    
+    target = m.group('target')
+    
+    if in1 and re.match('\d+', in1):
+      in1 = int(in1)
+    if in2 and re.match('\d+', in2):
+      in2 = int(in2)
+    
+    inputs = []
+    if in1 is not None:
+      inputs.append(in1)
+    inputs.append(in2)
+    
+    operation = Op(inputs=inputs, op=op, out=target)
+    wires[target] = operation
+    
+    line = f.readline()
+
+print(wires['a'])
+wires['b'] = 16076
+wires['a'].resolve(wires)
+print(wires['a'])
