81462a3c6e8e048248a6e668e0ad73637f924cee
Pavle Jonoski <jonoski.pavle@gmail>
2016-09-21 16:12:18 +0200

    Solution to day 14

---
+++ day-14/day14p2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import re
+
+from math import floor
+
+class Reindeer:
+  def __init__(self, name, speed, race_time, rest_time):
+    self.name = name
+    self.speed = speed
+    self.per_cycle_speed = speed*race_time
+    self.cycle_time = race_time + rest_time
+    self.race_time = race_time
+    self.rest_time = rest_time
+    self.score = 0
+  
+  def get_distance_after(self, time):
+    dist = floor(time/self.cycle_time)*self.per_cycle_speed
+    if time%self.cycle_time:
+      leftover_time = time - (floor(time/self.cycle_time)*self.cycle_time)
+      if leftover_time >= self.race_time:
+        dist += self.race_time*self.speed
+      else:
+        dist += leftover_time*self.speed
+    return dist
+  
+  def point(self):
+    self.score += 1
+
+reindeers = []
+
+with open('input') as f:
+  line = f.readline()
+  
+  while line:
+    line = line.strip()
+    
+    m = re.match('(?P<name>\w+) can fly (?P<speed>\d+) km/s for (?P<race_time>\d+) seconds, but then must rest for (?P<rest_time>\d+) seconds.', line)
+    name = m.group('name')
+    speed = int(m.group('speed'))
+    race_time = int(m.group('race_time'))
+    rest_time = int(m.group('rest_time'))
+    
+    reindeers.append(Reindeer(name=name, speed=speed, race_time=race_time, rest_time=rest_time))
+    
+    
+    line = f.readline()
+    
+
+for i in range(0, 2503):
+  lead = max(reindeers, key=lambda r: r.get_distance_after(i+1))
+  lead.point()
+
+winner = max(reindeers, key=lambda r: r.score)
+print(winner.name, winner.score)
+
+    
