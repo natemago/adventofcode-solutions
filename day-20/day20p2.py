f8360ee6f37b2bd6f3fd60fdf2be56b18a05c1fd
Pavle Jonoski <jonoski.pavle@gmail>
2016-09-22 20:52:31 +0200

    Day 20, part 2

---
+++ day-20/day20p2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import math
+
+def divisors(n):
+  divs = set()
+  for i in range(2,math.floor(math.sqrt(n))):
+    if n%i == 0:
+      divs.add(i)
+      divs.add(n/i)
+  divs.add(1)
+  divs.add(n)
+  return divs
+
+n = 1
+
+while True:
+  if sum(filter(lambda x: n/x <= 50, divisors(n)))*11 == 33100000:
+    print(n)
+    break
+  if n%1000 == 0:
+    print('not',n)
+  n += 1
